ce0ac5968aae1becedd755a72e1152c944eb5a9b
Roma <75065161+RomaYun323@users.noreply.github.com>
2026-09-18 16:40:41 +0800

    Revert "build: auto generate hant workspace"
    This reverts commit 435b61087c765eb11878262b9feb1e6c4a2b3719.

--- lua/wanxiang/set_schema.lua
+++ lua/wanxiang/set_schema.lua
@@ -172,8 +172,6 @@ local function translator(input, seg, env)
         "wanxiang_mixedcode.custom.yaml",
         "wanxiang_reverse.custom.yaml",
         "wanxiang_english.custom.yaml",
-        "wanxiang_abbrev.custom.yaml",
-        "wanxiang_phrase.custom.yaml",
         main_file,
     }
 
